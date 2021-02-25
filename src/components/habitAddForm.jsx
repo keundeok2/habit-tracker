8c26e074504af1c03ce0fe92698c147e156eb8ae
keundeok2 <keundeok2@gmail.com>
2021-02-25 22:38:11 +0900

    React Hooks

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,3 +1,35 @@
+import React, { memo } from "react";
+// rcc -> rsi
+const HabitAddForm = memo(props => {
+  const inputRef = React.createRef();
+  const formRef = React.createRef();
+
+  const onSubmit = event => {
+    event.preventDefault(); // 브라우저의 기본기능을 취소시킴 ( form이 submit 되면 화면이 전체 refresh )
+
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
+
+    inputRef.current.value = "";
+    // this.formRef.current.reset();
+  };
+
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="Habit"
+      ></input>
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
+
+export default HabitAddForm;
+
+/*
 import React, { PureComponent } from "react";
 
 class HabitAddForm extends PureComponent {
@@ -30,3 +62,4 @@ class HabitAddForm extends PureComponent {
 }
 
 export default HabitAddForm;
+*/
